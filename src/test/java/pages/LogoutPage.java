960af7be8301afb4c92b4ade638dfd2b1fd28fb4
Aditiya2487 <aditiya.209202191@muj.manipal.edu>
2025-09-03 22:39:56 +0530

    Initial comit - Selenium Capstone Project

---
+++ src/test/java/pages/LogoutPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LogoutPage extends BasePage {
+    @FindBy(id = "react-burger-menu-btn") private WebElement menuButton;
+    @FindBy(id = "logout_sidebar_link") private WebElement logoutLink;
+    public void logout() {
+        click(menuButton);
+        click(logoutLink);
+    }
+}
